8d7a7d0903a92d45e05f77c8a828bb213493d7de
Aman07a <aman16.aa17@gmail.com>
2024-01-29 15:53:32 +0100

    Making HTTP Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component, Injectable } from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
 
-@Injectable({ providedIn: 'root' })
-export class Car {
-  color = 'red';
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,15 +9,10 @@ export class Car {
 export class AppComponent {
   title = 'wsearch';
 
-  constructor(private car: Car, private wikipediaService: WikipediaService) {}
+  constructor(private wikipediaService: WikipediaService) {}
 
   onTerm(term: string) {
     const results = this.wikipediaService.search(term);
     console.log(results);
-    console.log('My car has a color of', this.car.color);
   }
 }
-
-const car = new Car();
-const wikipedia = new WikipediaService();
-new AppComponent(car, wikipedia);
