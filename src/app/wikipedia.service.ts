c1ca01f6a770ce1300f63f91d58968e72854b521
Aman07a <aman16.aa17@gmail.com>
2024-02-01 09:01:54 +0100

    Using TypeScript to Catch Errors

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,33 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-interface Car {
-  year: number;
-  color: string;
-  running: boolean;
-  make: {
-    name: string;
-    dateCreated: number;
-  };
-}
-
-const observable = new Observable<Car>((observer) => {
-  observer.next({
-    year: 200,
-    color: 'red',
-    running: true,
-    make: {
-      name: 'Chevy',
-      dateCreated: 20240201,
-    },
-  });
-}).pipe(map((car) => car.make.dateCreated));
-
-observable.subscribe((value) => {
-  console.log(value);
-});
 
 @Injectable({
   providedIn: 'root',
@@ -36,15 +9,17 @@ export class WikipediaService {
   constructor(private http: HttpClient) {}
 
   public search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf8: '1',
-        srsearch: term,
-        origin: '*',
-      },
-    });
+    return this.http
+      .get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*',
+        },
+      })
+      .pipe(map((response: any) => response.query.search));
   }
 }
